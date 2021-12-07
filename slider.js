dc09135482fa0b2440416310d77995de36bbd0b6
A11884 <95312385+A11884@users.noreply.github.com>
2021-12-07 16:44:15 +0500

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,12 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,3500);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+
+const rights =document.getElementById('rights').innerHTML = new Date().getFullYear()
